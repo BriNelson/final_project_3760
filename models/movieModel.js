1d2b8e21385e954ec7d64aa4b5d7cf1952c519f5
brain <ladofmusi@outlook.com>
2021-12-06 15:17:05 -0700

    model up to date, first route

--- models/movieModel.js
+++ models/movieModel.js
@@ -1,23 +1,18 @@
-import pkg from 'mongoose';
-const { model, Schema } = pkg;
-
-
+import pkg from 'mongoose'
+const { model, Schema } = pkg
 
 const movieSchema = new Schema({
-    
-    title: String,
-    key: Number,
-    summary: String,
-    network: String,
-    imgUrl: String,
-    genere: Array,
-    score: Number,
-    haveRead: Boolean,
-    wantRead: Boolean,
 
-
+  title: String,
+  key: Number,
+  summary: String,
+  network: String,
+  imgUrl: String,
+  genere: Array,
+  score: Number,
+  favorite: Boolean,
+  wantWatch: Boolean
 
 })
 
-
-export default model("movieSchema", movieSchema);
+export default model('movieSchema', movieSchema)
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
-//TODO: SCHEMA FILE import bookSchema from './models/bookModel.js'
-//TODO: config file
+import movieSaveSchema from './models/movieModel.js'
+// TODO: config file
 import { port, mongoUri } from './config.js'
 console.log(`Your port is ${port}`)
 const app = express()
@@ -21,3 +21,33 @@ mongoose.connect(mongoUri, {
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public')) /// /enviorment variable needs to go here
+
+
+app.post('/haveWatched', (req, res) => {
+  // const todo = req.body
+  const movieSave = new movieSaveSchema({
+
+    title: req.body.title,
+    key: req.body.key,
+    summary: req.body.summary,
+    network: req.body.network,
+    imgUrl: req.body.imgUrl,
+    genere: req.body.genere,
+    score: req.body.score,
+    wantWatch: req.body.wantWatch,
+    favorite: req.body.favorite
+
+  })
+  movieSave.save().then((result) => { console.log(result) })
+})
+
+
+
+
+
+if (port == null || port == '') {
+  port = 8000
+}
+
+
+app.listen(port)
